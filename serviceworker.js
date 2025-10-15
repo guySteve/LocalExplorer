62b35996819d61f379ab3e708e418b5c8c8c06fe
StephenMOhamed <122633182+guySteve@users.noreply.github.com>
2025-10-14 23:41:47 -0400

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,71 @@
+const CACHE_NAME = 'local-explorer-v1';
+const urlsToCache = [
+    '/',
+    'local_explorer.html',
+    'https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap'
+    // Note: Google Maps and other external API scripts cannot be cached by the service worker.
+];
+
+// Install the service worker and cache static assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Clean up old caches when a new service worker is activated
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Serve cached content when offline, or fetch from network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // Not in cache - fetch from network
+                return fetch(event.request).then(
+                    response => {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
