c6ed10cd56150b3098fd8956eeac38a307a4906c
StephenMOhamed <122633182+guySteve@users.noreply.github.com>
2025-10-18 22:55:38 -0400

    Create key.js

---
+++ js/key.js
@@ -0,0 +1,5 @@
+// Google Maps and Places API key
+window.MAPS_API_KEY = 'AIzaSyB9PMHJVDip9WvIQVywmRjcdhqiQPrtXiY';
+
+// Ticketmaster Discovery API key
+window.TICKETMASTER_API_KEY = 'XmzfrRHZilGDGfD63SmdamF288GZ3FxH';
