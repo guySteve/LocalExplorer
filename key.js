7846175e32bf224438870646b776d68f792be138
StephenMOhamed <122633182+guySteve@users.noreply.github.com>
2025-10-25 19:15:26 -0400

    Delete key.js

--- key.js
+++
@@ -1,5 +0,0 @@
-// Google Maps and Places API key
-window.MAPS_API_KEY = 'AIzaSyB9PMHJVDip9WvIQVywmRjcdhqiQPrtXiY';
-
-// Ticketmaster Discovery API key (optional)
-window.TICKETMASTER_API_KEY = 'XmzfrRHZilGDGfD63SmdamF288GZ3FxH';
